835817b7cfaf67094de781b0d6d5771124bb7173
Tyskip <tbskipworth@gmail.com>
2025-07-23 17:24:45 -0500

    Update memberships.js

--- api/memberships.js
+++ api/memberships.js
@@ -1,3 +1,14 @@
 export default async function handler(req, res) {
-  res.status(200).json({ message: "GymMaster Proxy is working!" });
+  const API_KEY = process.env.GYMMASTER_API_KEY;
+
+  const response = await fetch("https://humbabeathletics.gymmasteronline.com/portal/api/v1/memberships", {
+    headers: {
+      "Authorization": `key ${API_KEY}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await response.json();
+
+  res.status(response.status).json(data);
 }
